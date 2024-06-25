9609d0bf59f6acd1aae27b0f8b031cb6a9c92b7a
WinniHuang <usj0326@gmail.com>
2024-06-25 18:39:58 +0800

    chore: add tailwindCSS settings

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}", "./index.html"],
+  theme: {
+    fontSize: {
+      sm: ["12px", "16px"],
+      base: ["14px", "20px"],
+      lg: ["16px", "24px"],
+      xl: ["20px", "28px"],
+      "2xl": ["24px", "30px"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
