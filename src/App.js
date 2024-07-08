56e081e6813867b273ad1d0623fd402a51faab87
WinniHuang <usj0326@gmail.com>
2024-07-08 12:48:16 +0800

    fix: react/react-in-jsx-scope eslint rules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from "react";
 import Banner from "./components/banner";
 import Contents from "./components/contents";
 import ThemedMode from "./components/themeMode";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App text-base">
       <ThemedMode />
@@ -12,5 +13,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
