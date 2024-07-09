05df655e92b372d8056b32adc37e154a6f559b72
WinniHuang <usj0326@gmail.com>
2024-07-09 17:09:18 +0800

    feat: adjust photo stickers and layout

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Winni from "../images/remove_background_winni.png";
+import Winni from "../images/winni2024.png";
 
 const info = [
   {
@@ -20,27 +20,33 @@ const Link: React.FC<{ href: string; content: string }> = ({
   href,
   content,
 }) => (
-  <a href={href} className="text-xs btn-custom">
+  <a href={href} className="btn-custom">
     {content}
   </a>
 );
 
 export default function Banner() {
   return (
-    <section className="banner-section sm:flex-row">
-      <div className="flex justify-center border-custom sm:flex-[20%] sm:flex-grow-0 sm:border-r-none">
-        <img className="w-4/6 sm:w-full" src={Winni} alt="Winni Huang" />
+    <section className="p-3 border-custom banner-section sm:flex-row">
+      <div className="flex justify-center sm:flex-[0_0_20%] lg:flex-[0_0_15%]">
+        <img
+          className="h-1/3 w-1/3 rounded-full sm:h-full sm:w-full"
+          src={Winni}
+          alt="Winni Huang"
+        />
       </div>
-      <div className="flex flex-1 flex-col items-start justify-start p-5 border-custom border-t-none sm:border-t">
-        <h1 className="text-2xl font-bold dark:text-orange-400">
+      <div className="flex flex-1 flex-col items-start justify-start p-5 pb-2">
+        <h1 className="text-xl font-bold sm:text-2xl dark:text-orange-400">
           黃汶鈞 Winni Huang :)
         </h1>
-        <h3 className="mt-2 text-lg dark:text-stone-300">前端工程師</h3>
-        <div className="mt-3 flex flex-col sm:flex-row">
+        <h3 className="mt-1 text-base sm:text-lg dark:text-stone-300">
+          前端工程師
+        </h3>
+        <div className="flex flex-col sm:flex-row">
           {info.map(({ href, content }, index, array) => (
             <div
               key={href}
-              className={`mt-3 ${index < array.length - 1 ? "sm:mr-2" : ""}`}
+              className={`mt-2 ${index < array.length - 1 ? "sm:mr-2" : ""}`}
             >
               <Link href={href} content={content} />
             </div>
